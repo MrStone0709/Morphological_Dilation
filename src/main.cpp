d329c3ec9b359bedf31801630c861178be7b6432
Mr_Stone0709 <a13007098088@163.com>
2026-09-11 19:52:18 +0800

    完成演示程序

--- src/main.cpp
+++ src/main.cpp
@@ -7,38 +7,40 @@
 
 int main() {
 
-  std::vector<std::vector<int>> imageData = {{0, 0, 0, 0, 0},
-                                             {0, 1, 1, 0, 0},
-                                             {0, 1, 1, 1, 0},
-                                             {0, 0, 0, 0, 0},
-                                             {0, 0, 0, 0, 0}};
+  std::vector<std::vector<int>> imageData = {
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
+      {0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
+      {0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
+      {0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
+      {0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-  // 创建 5×5 图像
+  // 创建 10×10 图像
   BinaryImage image(imageData);
 
   std::cout << "Original image:" << std::endl;
   image.print();
 
-  // 创建一个不对称结构元素
-  std::vector<std::vector<int>> matrix = {{0, 1, 1}, {0, 1, 0}, {1, 0, 0}};
+  // 创建5*5矩形结构元素，锚点在中心
+  std::vector<std::vector<int>> square = {{1, 1, 1, 1, 1},
+                                          {1, 1, 1, 1, 1},
+                                          {1, 1, 1, 1, 1},
+                                          {1, 1, 1, 1, 1},
+                                          {1, 1, 1, 1, 1}};
 
-  StructuringElement element(matrix, 1, 1);
-
-  std::cout << std::endl;
-  std::cout << "Original structuring element:" << std::endl;
-
-  element.print();
-
-  // 查看 180°反射后的结构元素
-  StructuringElement reflected = element.reflected();
+  StructuringElement square_element(square, 2, 2);
 
   std::cout << std::endl;
-  std::cout << "Reflected structuring element:" << std::endl;
+  std::cout << "Square structuring element:" << std::endl;
 
-  reflected.print();
+  square_element.print();
 
   // 膨胀
-  BinaryImage dilated = Morphology::dilation(image, element);
+  BinaryImage dilated = Morphology::dilation(image, square_element);
 
   std::cout << std::endl;
   std::cout << "Dilated image:" << std::endl;
@@ -46,12 +48,42 @@ int main() {
   dilated.print();
 
   // 腐蚀
-  BinaryImage eroded = Morphology::erosion(image, element);
+  BinaryImage eroded = Morphology::erosion(image, square_element);
 
   std::cout << std::endl;
   std::cout << "Eroded image:" << std::endl;
 
   eroded.print();
 
+  // 创建5*5圆形结构元素，锚点在中心
+  std::vector<std::vector<int>> circle = {{0, 0, 1, 0, 0},
+                                          {0, 1, 1, 1, 0},
+                                          {1, 1, 1, 1, 1},
+                                          {0, 1, 1, 1, 0},
+                                          {0, 0, 1, 0, 0}};
+
+  StructuringElement circle_element(circle, 2, 2);
+
+  std::cout << std::endl;
+  std::cout << "Circle structuring element:" << std::endl;
+
+  circle_element.print();
+
+  // 膨胀
+  dilated = Morphology::dilation(image, circle_element);
+
+  std::cout << std::endl;
+  std::cout << "Dilated image:" << std::endl;
+
+  dilated.print();
+
+  // 腐蚀
+  eroded = Morphology::erosion(image, circle_element);
+
+  std::cout << std::endl;
+  std::cout << "Eroded image:" << std::endl;
+
+  eroded.print();
+  
   return 0;
 }
